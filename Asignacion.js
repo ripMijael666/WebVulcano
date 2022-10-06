5b3299e66c7c558bbfcaa3610b2855cf1dc5a0e5
Mijael Rocha <josemijaelrochavera666@gmail.com>
2022-10-06 16:21:29 -0400

    CLASES DE JAVASCRIPT JUNIOR-1

---
+++ Asignacion.js
@@ -0,0 +1,21 @@
+// OPERADORES DE ASIGNACION 
+let asignacion = 60;
+    asignacion += 6;
+document.write(asignacion); '\n'
+
+let asignacionDos = 60;
+    asignacionDos -= 6;
+document.write(asignacion);
+
+let asignacionTres = 60;
+    asignacionTres *= 6;
+document.write(asignacion);
+
+let asignacionCuatro = 60;
+    asignacionCuatro /= 6;
+document.write(asignacion);
+
+let asignacionCinco = 640;
+    asignacionCinco %= 6;
+document.write(asignacion);
+
---
+++ Junior.js
@@ -0,0 +1,44 @@
+// TIPOS DE DATOS EN JAVASCRIPT
+    string = "Mijael"
+    number = 19
+    booleano =  true
+
+
+// FORMAS DE DECLARAR UNA VARIABLE 
+    var nombre = "Mijael";
+        nombre = "Jose";
+    alert(nombre);
+    let apellido = "Rocha";
+        apellido = "Vera";
+    alert(apellido);
+    // LAS VARIABLES var Y let SI PUEDEN CAMBIAR SUS VALORES 
+    const edad  = 18;
+    alert(edad);
+    // LAS VARIABLES CONSTANTES "const" NO PUEDEN CAMBIAR SU VALOR 
+    // SE DEFINE UNA VES Y NO SE PUEDEN DEFINIR O CAMBIAR SU VALOR NUNCA MAS 
+
+
+// NULL
+    let numero = null; 
+    // AL DECLARAR NULL A UNA VARIABLE ESTA SE CONVIERTE A UNA VARIABLE VACIA "SU VALOR ES VACIO"
+
+
+
+// + * - / Y NAN
+    let numeroUno = 5;
+    let numeroDos = 5;
+    alert(numeroUno * numeroDos);
+
+    let nan = "Mijael";
+    alert(numeroUno + nan);
+    // AQUI SE ESTA CONCATENANDO 
+    alert(numeroUno * nan);
+    // AQUI YA ES NAN "NOT A NUMBER" "NO ES UN NUMERO" NO SE PUEDE MULTIPLICAR NUMERO CON TEXTO
+
+
+// "promt" alerta en la pantalla interactiva 
+    let promptUno = prompt("Ingrese su Nombre");
+    let promptDos = prompt("Ingrese su Apellido");
+    alert("Bienvenido :" + promptUno + promptDos);
+
+
